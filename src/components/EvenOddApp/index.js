d62e36a09d32e222c34237c80b38baaf04aa59f8
Selvashyamalan <selvamiitindia@gmail.com>
2023-12-14 12:03:16 +0000

    Components

---
+++ src/components/EvenOddApp/index.js
@@ -0,0 +1,36 @@
+import './index.css'
+
+import {Component} from 'react'
+
+class EvenOddApp extends Component {
+  state = {
+    count: 0,
+  }
+
+  getRandomNumber = () => Math.ceil(Math.random() * 100)
+
+  onIncrement = () => {
+    const randomNumber = this.getRandomNumber()
+
+    this.setState(prevState => ({count: prevState.count + randomNumber}))
+  }
+
+  render() {
+    const {count} = this.state
+    const evenOrOdd = count % 2 === 0 ? 'Even' : 'Odd'
+    return (
+      <div className="container">
+        <div className="even-container">
+          <h1 className="heading">Count {count}</h1>
+          <p className="desc">Count is {evenOrOdd}</p>
+          <button className="button" type="button" onClick={this.onIncrement}>
+            Increment
+          </button>
+          <p className="imp">*Increase By Random Number Between 0 to 100</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
